8c8328284a68a971964a776a2a8cb00c9cbf5bfa
Illarion <hostingerak@mail.ru>
2020-04-21 11:44:16 +0700

    Stable version!

--- PasswordDefender/Model/DataFileManager.cs
+++ PasswordDefender/Model/DataFileManager.cs
@@ -1,23 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
+using System;
 using System.IO;
-using Newtonsoft.Json;
 
 namespace PasswordDefender.Model
 {
     static class DataFileManager // Класс, предоставляющий методы сохранения и получения зашифрованных данных
     {
-        public static string DataFilesDirectory { get; set; } = $@"{Environment.CurrentDirectory}\data\";
-        public static void SaveDataToFile(Data dataToSave) 
+        public static readonly string DataFilesDirectory = $@"{Environment.CurrentDirectory}\data\";
+        //private static string[] occupiedFileNames = File.
+        public static void SaveDataToFile(Data dataToSave)
         {
             string dataInJSONToSave = JsonConvert.SerializeObject(dataToSave);
 
-            using (TextWriter writeEncryptedDataToFileStream = new StreamWriter($@"{DataFilesDirectory}{dataToSave.GetHashCode()}"))
+            using (TextWriter writeEncryptedDataToFileStream = new StreamWriter($@"{DataFilesDirectory}{dataToSave.Id}"))
                 writeEncryptedDataToFileStream.WriteLine(dataInJSONToSave);
 
+        }
+
+        public static void DeleteData(Data dataToDelete)
+        {
+            File.Delete(DataFilesDirectory + dataToDelete.Id);
         }
 
         public static Data[] GetAllData()
@@ -27,21 +29,19 @@ namespace PasswordDefender.Model
 
             Data[] allDownloadedData = new Data[encryptedDataPaths.Length];
 
-            RijndaelCryptographer rijndaelCryptographer = new RijndaelCryptographer();
-
-                for (int i = 0; i <= encryptedDataPaths.Length - 1; i++)
+            for (int i = 0; i <= encryptedDataPaths.Length - 1; i++)
+            {
+                using (TextReader readEncryptedDataFromDirectoryStream = new StreamReader(encryptedDataPaths[i]))
                 {
-                    using (TextReader readEncryptedDataFromDirectoryStream = new StreamReader(encryptedDataPaths[i]))
-                    {
-                        Data downloadedData = JsonConvert.DeserializeObject<Data>(readEncryptedDataFromDirectoryStream.ReadToEnd());
+                    Data downloadedData = JsonConvert.DeserializeObject<Data>(readEncryptedDataFromDirectoryStream.ReadToEnd());
 
 
-                        rijndaelCryptographer.DecryptData(downloadedData);
+                    RijndaelCryptographer.DecryptData(downloadedData);
 
 
-                        allDownloadedData[i] = downloadedData;
-                    }
+                    allDownloadedData[i] = downloadedData;
                 }
+            }
 
             return allDownloadedData;
         }
